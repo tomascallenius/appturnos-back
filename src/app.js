abc389550f1032d15e1349f4e45039f07be807d4
F.Fernandez <hellsingsd@gmail.com>
2023-11-27 18:23:57 -0300

    basic app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require("cors");
+const routes = require('./routes/index.js')
 
 const server = express();
+server.name = "API";
+server.use(express.json());
+server.use(morgan("dev"));
+server.use(cors());
+server.use("/", routes);
 
 module.exports = server;
