0028514d784dbd2256e775af54220f65897d8e71
F.Fernandez <hellsingsd@gmail.com>
2023-11-27 23:05:54 -0300

    Conexion exitosa a MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
- const server = require('./src/app.js')
+require('dotenv').config();
+const server = require('./src/app.js')
+require('./src/DB/db.js')
 
   server.listen(3001, () => {
     console.log("Server listening on 3001"); 
---
+++ src/DB/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { MONGO_URI } = process.env;  // 
+
+mongoose.connect(MONGO_URI);  // establece conexion cn el mi propio pc y en el puerto 27017
+const db = mongoose.connection; // guarda la conexion en la const db para exportarla
+
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a MongoDB');
+});
+// .on y .once son manejadores de evento, funcionan diferente al .then de siempre
+
+module.exports = db;
