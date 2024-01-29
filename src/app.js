340d8e16fed29e1b95ffcd40b28f39f4e331b450
tomascallenius <tomascallenius96@gmail.com>
2024-01-29 11:29:19 -0300

    volviendo atras

--- src/app.js
+++ src/app.js
@@ -7,18 +7,18 @@ const server = express();
 server.name = "API";
 server.use(express.json());
 server.use(morgan("dev"));
-// server.use(cors());
+server.use(cors());
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/", routes);
 
