ead600d68e7f1858e24110336b86cd4eeb145d45
tomascallenius <tomascallenius96@gmail.com>
2024-01-29 11:09:39 -0300

    descomente las cors

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
-const routes = require('./routes/index.js')
+const routes = require("./routes/index.js");
 
 const server = express();
 server.name = "API";
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
+// server.use(cors());
 
-/* server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-  });
- */
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/", routes);
 
