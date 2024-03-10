0c41800846de9431f8c6e414ca6e61a3d57b2828
tomascallenius <tomascallenius96@gmail.com>
2024-03-09 21:27:36 -0300

    reformulando db e index para deploy

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+// require("dotenv").config();
+// const server = require("./src/app.js");
+// require("./src/DB/db.js");
+
+// const PORT = process.env.PORT;
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on port: ${PORT}`);
+// });
+
 require("dotenv").config();
 const server = require("./src/app.js");
-require("./src/DB/db.js");
+const connectDB = require("./src/DB/db.js"); // Importa la función para conectar con la base de datos
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    // Llama a la función para conectar con la base de datos
+    server.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
--- src/DB/db.js
+++ src/DB/db.js
@@ -1,14 +1,30 @@
+// const mongoose = require("mongoose");
+
+// const { MONGO_URI } = process.env; //
+
+// mongoose.connect(MONGO_URI); // establece conexion cn el mi propio pc y en el puerto 27017
+// const db = mongoose.connection; // guarda la conexion en la const db para exportarla
+
+// db.on("error", console.error.bind(console, "Error de conexión a MongoDB:"));
+// db.once("open", () => {
+//   console.log("Conexión exitosa a MongoDB");
+// });
+// // .on y .once son manejadores de evento, funcionan diferente al .then de siempre
+
+// module.exports = db;
+
 const mongoose = require("mongoose");
 
 const { MONGO_URI } = process.env; //
 
-mongoose.connect(MONGO_URI); // establece conexion cn el mi propio pc y en el puerto 27017
-const db = mongoose.connection; // guarda la conexion en la const db para exportarla
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI); // establece conexion con la URI de MongoDB
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error de conexión a MongoDB:", error.message);
+    throw error;
+  }
+};
 
-db.on("error", console.error.bind(console, "Error de conexión a MongoDB:"));
-db.once("open", () => {
-  console.log("Conexión exitosa a MongoDB");
-});
-// .on y .once son manejadores de evento, funcionan diferente al .then de siempre
-
-module.exports = db;
+module.exports = connectDB;
